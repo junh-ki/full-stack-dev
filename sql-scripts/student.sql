854bc0fc97765d735e8d2dc57348e36c786726f1
junh-ki <kijoonh91@gmail.com>
2021-02-15 20:27:21 +0100

    a sql script added and modified

--- sql-scripts/student.sql
+++ sql-scripts/student.sql
@@ -3,10 +3,10 @@ create database projectdb
 use projectdb
 
 create table studenttab(
-id int PRIMARY KEY AUTO_INCREMENT,
-sname varchar(20),
-scourse varchar(30),
-sfee int
+    id int PRIMARY KEY AUTO_INCREMENT,
+    sname varchar(20),
+    scourse varchar(30),
+    sfee int
 )
 
 select * from studenttab
