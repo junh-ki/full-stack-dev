bc99e1e83465edd346759f34577a6fd070fa05ad
junh-ki <kijoonh91@gmail.com>
2021-03-29 13:20:32 +0200

    FlightReservation with DB schema added, Entities defined w/ annotations
    Change-Id: Ifcc0a2c017f81725cf356eb8a005c04bec4b7bb6
    Signed-off-by: junh-ki <kijoonh91@gmail.com>

---
+++ flightreservation/src/main/java/com/jun/flightreservation/entities/AbstractEntity.java
@@ -0,0 +1,23 @@
+package com.jun.flightreservation.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass // this class is not directly mapped to the data table, instead it acts as a parent class for all the other entities
+public class AbstractEntity {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO) // this annotation says that this id field will be automatically incremented in the database for us
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
