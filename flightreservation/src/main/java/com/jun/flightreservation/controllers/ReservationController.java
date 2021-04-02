fada06d0ff481d43136b54b3610ab039423e8d6c
junh-ki <kijoonh91@gmail.com>
2021-04-02 17:37:53 +0200

    Search Flights and Complete Reservation implemented and UI tested
    Change-Id: Iae7706490671061d6366b912eece628ea890fb64
    Signed-off-by: junh-ki <kijoonh91@gmail.com>

---
+++ flightreservation/src/main/java/com/jun/flightreservation/controllers/ReservationController.java
@@ -0,0 +1,25 @@
+package com.jun.flightreservation.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jun.flightreservation.entities.Flight;
+import com.jun.flightreservation.repos.FlightRepository;
+
+@Controller
+public class ReservationController {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    @RequestMapping("/showCompleteReservation")
+    public String showCompleteReservation(@RequestParam("flightId") Long flightId, ModelMap modelMap) {
+        Flight flight = flightRepository.findById(flightId).get();
+        modelMap.addAttribute("flight", flight);
+        return "completeReservation";
+    }
+
+}
--- flightreservation/src/main/webapp/WEB-INF/jsps/displayFlights.jsp
+++ flightreservation/src/main/webapp/WEB-INF/jsps/displayFlights.jsp
@@ -15,6 +15,7 @@
 <tr>
 <th>Airlines</th>
 <th>Departure City</th>
+<th>Arrival City</th>
 <th>Departure Time</th>
 </tr>
 
@@ -23,6 +24,7 @@
 <td>${flight.operatingAirlines}</td>
 <td>${flight.departureCity}</td>
 <td>${flight.arrivalCity}</td>
+<td>${flight.estimatedDepartureTime}</td>
 <td><a href="showCompleteReservation?flightId=${flight.id}">Select</a></td>
 </tr>
 </c:forEach>
