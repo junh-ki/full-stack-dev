f52ab73ac451b5f3e85c0eeed733549158442fc5
junh-ki <kijoonh91@gmail.com>
2021-04-07 19:17:50 +0200

    startCheckIn.jsp, handling the jsp, reservationConfirmation.jsp done
    Change-Id: I0b82c3d98a6800b3d4ed33eb8fa333fab95daa3b
    Signed-off-by: junh-ki <kijoonh91@gmail.com>

--- flightcheckin/src/main/java/com/jun/flightcheckin/integration/ReservationRestClientImpl.java
+++ flightcheckin/src/main/java/com/jun/flightcheckin/integration/ReservationRestClientImpl.java
@@ -1,19 +1,22 @@
 package com.jun.flightcheckin.integration;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.jun.flightcheckin.integration.dto.Reservation;
 import com.jun.flightcheckin.integration.dto.ReservationUpdateRequest;
 
+@Component
 public class ReservationRestClientImpl implements ReservationRestClient {
 
-    private static final String RESERVATION_REST_URL = "localhost:8080/flightreservation/reservations/";
+    private static final String RESERVATION_REST_URL = "http://localhost:8080/flightreservation/reservations/";
 
     @Override
     public Reservation findReservation(Long id) {
         RestTemplate restTemplate = new RestTemplate();
-        Reservation reservation = restTemplate
-                .getForObject(RESERVATION_REST_URL + id, Reservation.class);
+        
+        String url = RESERVATION_REST_URL + id;
+        Reservation reservation = restTemplate.getForObject(url, Reservation.class);
         return reservation;
     }
 
