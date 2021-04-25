4f5c290f1eb3b16e4b77a6980eddb6595ea56f7c
junh-ki <kijoonh91@gmail.com>
2021-04-25 23:56:59 +0200

    angular start button doesn't work => routing issue

--- checkinapp/src/app/app.module.ts
+++ checkinapp/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { StartcheckinComponent } from './components/startcheckin/startcheckin.component';
 import { CheckinComponent } from './components/checkin/checkin.component';
 import { ConfirmcheckinComponent } from './components/confirmcheckin/confirmcheckin.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +19,11 @@ import { ConfirmcheckinComponent } from './components/confirmcheckin/confirmchec
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- checkinapp/src/app/services/data.service.ts
+++ checkinapp/src/app/services/data.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url:string = "http://localhost:8080/flightreservation/reservations/";
+
+  constructor(private _http:HttpClient) { }
+
+  public getReservation(id:number):any{
+    return this._http.get(this.url+id)
+    .pipe(map(response=>{
+      return response;
+    },
+    error=>{
+      console.error(error);
+    }
+  ))
+  }
+
+  public checkin(checkInRequest):any{
+    return this._http.post(this.url,checkInRequest).pipe(map(res=>{
+      return res;
+    },
+    error=>{
+      console.error(error);
+    }
+  ))
+  }
 }
