436eb4201d1e4fdf21fda2d3508333a14e4a7834
Junhyung Ki <kijoonh91@gmail.com>
2021-05-02 20:03:24 +0200

    Add Patient, Toastify implemented and tested

--- reactprojects/clinicalsapp/src/components/CollectClinicals.js
+++ reactprojects/clinicalsapp/src/components/CollectClinicals.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 
 class CollectClinicals extends React.Component{
+
+    state = {}
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/clinicalservices/api/patients/'+this.props.match.params.patientId)
+        .then(res=>{
+            this.setState(res.data)
+        })
+    }
+
     render() {
         return(<div>
-
+            <h2>Patient Details:</h2>
+            First Name: {this.state.firstName}
+            Last Name: {this.state.lastName}
+            Age: {this.state.age}
+            <form>
+                
+            </form>
         </div>)
     }
 }
