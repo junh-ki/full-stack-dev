eb0a7cd19d6659135df74eeb07fa689f312e30cd
Junhyung Ki <kijoonh91@gmail.com>
2021-05-03 23:20:44 +0200

    CollectClinicals and AnalyzeData implemented and tested

--- reactprojects/clinicalsapp/src/components/CollectClinicals.js
+++ reactprojects/clinicalsapp/src/components/CollectClinicals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 class CollectClinicals extends React.Component{
 
@@ -12,14 +13,35 @@ class CollectClinicals extends React.Component{
         })
     }
 
-    render() {
+    handleSubmit(event) {
+        event.preventDefault();
+        const data={
+            patientId:this.props.match.params.patientId,
+            componentName:this.componentName,
+            componentValue:this.componentValue
+        }
+        axios.post("http://localhost:8080/clinicalservices/api/clinicals/",data)
+        .then(res=>{
+            toast("Patient Data Saved Successfully",{autoClost:3000,position:toast.POSITION.BOTTOM_CENTER})
+        })
+    }
+
+    render(event) {
         return(<div>
             <h2>Patient Details:</h2>
             First Name: {this.state.firstName}
             Last Name: {this.state.lastName}
             Age: {this.state.age}
+            <h2>Patient Clinical Data:</h2>
             <form>
-                
+                Clinical Entry Type <select onChange={(event)=>{this.componentName=event.target.value}}>
+                    <option>Select One</option>
+                    <option value="bp">Blood Pressure(Sys/Dys)</option>
+                    <option value="hw">Height/Weight</option>
+                    <option value="heartRate">Heart Rate</option>
+                </select>
+                Value:<input type="text" name="componentValue" onChange={(event)=>{this.componentValue=event.target.value}}/>
+                <button onClick={this.handleSubmit.bind(this)}>Confirm</button>
             </form>
         </div>)
     }
